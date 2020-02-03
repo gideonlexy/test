8f51caab946f2ed1bdf17a5a96f5a0305a5555b7
gideonlexy <gideon.lexy@tuzidi.org>
2020-02-03 19:21:57 +0300

    chore: modified the app with the changes raised from the chore#1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
-import Card from './components/card/card.component'
+import Card from './components/card/card.component';
 
 import './App.css';
 
-class App extends Component {
-  state = {
-    
-  }
-
-  render() {
-    
-    return (
-      <div>
-        <nav className='nav'>
-          <h2 className='wrapper'>InnRoad</h2>
-        </nav>
-        <div className='wrapper'>
-          <Card />
-        </div>
-      </div>
-    );
-  }
-
-}
+const App = () => (
+	<div>
+		<nav className="nav">
+			<h2 className="wrapper">InnRoad</h2>
+		</nav>
+		<div className="wrapper">
+			<Card />
+		</div>
+	</div>
+);
 
 export default App;
